420253fe27eb808d56aa569ba413d6b5e4b542ce
mayank-yd <mayank.yadav@tudip.com>
2024-02-15 16:46:18 +0530

    First commit

--- pdf/views.py
+++ pdf/views.py
@@ -8,17 +8,18 @@ import io
 # Create your views here.
 def accept(request):
     if request.method == "POST":
-        name = request.POST.get("name","")
-        phone = request.POST.get("phone","")
-        email = request.POST.get("email", "")
-        school = request.POST.get("school", "")
-        degree = request.POST.get("degree", "")
-        university = request.POST.get("university", "")
-        skills = request.POST.get("skills", "")
-        summary = request.POST.get("summary", "")
-        workexperince = request.POST.get("workexperience", "")
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        email = request.POST.get("email")
+        school = request.POST.get("school")
+        degree = request.POST.get("degree")
+        university = request.POST.get("university")
+        skills = request.POST.get("skills")
+        summary = request.POST.get("summary")
+        workexperince = request.POST.get("workexperience")
 
         profile = Profile(name=name, phone=phone, email=email, school=school, degree=degree, university=university, skills=skills, summary=summary, workexperince=workexperince)
+        print(profile)
         profile.save()
     return render(request,"accept.html")
 
@@ -38,5 +39,5 @@ def resume(request,id):
 
 def list(request):
     profile=Profile.objects.all()
-    pro=4
+    
     return render(request, "list.html", {'profile': profile})
